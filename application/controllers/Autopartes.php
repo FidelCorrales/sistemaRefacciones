1198c2571b1445d3e8553ffdeeffd8a27eee5751
unknown <ffcorralesandrade@gmail.com>
2019-07-19 18:36:37 -0500

    controlador v0

--- application/controllers/Autopartes.php
+++ application/controllers/Autopartes.php
@@ -6,52 +6,140 @@ class Autopartes extends CI_Controller{
     function __construct()
     {
         parent::__construct();
+        $this->load->database();
         $this->load->model('autopartes_m');
     }
     
     public function index(){
-
-        //$this->load->model('Autopartes_model');
-        $this->load->view('principal.html');
+        $this->load->view('principal',$data);
     }
 
-    /////////////////Tipos/////////////////
-    public function dame_tipo_autoparte(){
-        echo("Método no implementado");
+    //---CRUD---//
+    //Crear
+    public function insertar_autoparte(){
+        
+        $marca = $_POST['marca'];
+        $costo = $_POST['costo'];
+        $descripcion = $_POST['descripcion'];
+        $compatibilidad = $_POST['compatibilidad'];
+        $tipo = $_POST['tipo'];
+        $unidad = $_POST['unidad'];
+
+        $confirmacion = $this->autopartes_m->nuevaAutoparte($marca,$costo,$descripcion,$compatibilidad,$tipo,$unidad);
+        if(!is_null($confirmacion)){
+            return $confirmacion;
+        }
+        else{
+            $msjError = "No se logro guardar en la base de datos, intenté de nuevo.";
+            return $msjError;
+        }
     }
+
+    //leer
+    public function leer_autopartes(){
+        $autopartes = $this->autopartes_m->leerAutopartes();
+        if(!empty($autopartes)){
+            $data['autopartes'] = $autopartes;
+            $this->load->view('principal.html',$data);
+        }else{
+            $msjResult = "No se logro leer en la base de datos, intenté de nuevo.";
+            return $msjResult;
+        }
+    }
+
+    //Actualizar
+    public function actualizar_suspencion(){
+        $actualizacion = $this->autopartes_m->actualizarAutoparte();
+        $msjResult;
+        if(!empty($actualizacion)){
+            $msjResult = "Se logro actualizar la autoparte.";
+            return $msjResult;
+        }else{
+            $msjResult = "Se logro actualizar, intenté de nuevo.";
+            return $msjResult;
+        }
+    }
+
+    //Eliminar 
+    public function eliminar_autoparte(){
+        $confirmacion = $this->autopartes_m->eliminarAutoparte($id);
+        if(!is_null($confirmacion)){
+            return $confirmacion;
+        }
+        else{
+            $msjError = "No se logro eliminar en la base de datos, intenté de nuevo.";
+            return $msjError;
+        } 
+    }
+    //---FIN CRUD---//
 
     /////////////////Categorias/////////////////
 
-    //Suspencion
-    public function dame_aut_int(){
+    //---Suspension---//
+    public function dame_suspencion(){
         $autoparte_suspension = $this->autopartes_m->obtenerAutoparteSuspension();
         if (!empty($autoparte_suspension)){
-            echo '<pre>';
-            print_r($autoparte_suspension);
-            echo '</pre>';    
+            // echo '<pre>';
+            // print_r($autoparte_suspension);
+            // echo '</pre>';   
+            $data['suspension'] = $autoparte_suspension;
+            $this->load->view('principal.html',$data);
+            
         }else{
-            echo "No se encontraron autopartes de suspensión";
+             echo "No se encontraron autopartes de suspensión";
         }  
     }
 
-    //Partes externas motor
+    //---Partes externas motor---//
     public function dame_aut_ext(){
-
+        $autoparte_ext = $this->autopartes_m->obtenerAutoparteExt();
+        if (!empty($autoparte_ext)){   
+            $data['extmotor'] = $autoparte_ext;
+            $this->load->view('principal.html',$data);
+        }else{
+            $msjError; 
+            $msjError = "No se encontraron autopartes externas de motor";
+            return $msjError;
+        }  
     }
     
-    //Frenos
+    //---Frenos---//
     public function dame_frenos(){
-
+        $autoparte_freno = $this->autopartes_m->obtenerFrenos();
+        if (!empty($autoparte_freno)){   
+            $data['frenos'] = $autoparte_freno;
+            $this->load->view('principal.html',$data);
+        }else{
+            $msjError; 
+            $msjError = "No se encontraron frenos";
+            return $msjError;
+        } 
     }
 
-    //Herramienta
+    //---Herramienta---//
     public function dame_herramienta(){
-
+        $autoparte_herram = $this->autopartes_m->obtenerHerramientas();
+        if (!empty($autoparte_herram)){   
+            $data['herramientas'] = $autoparte_herram;
+            $this->load->view('principal.html',$data);
+        }else{
+            $msjError; 
+            $msjError = "No se encontraron herramientas";
+            return $msjError;
+        } 
     }
 
-    //Accesorios
+    //---Accesorios---//
     public function dame_accesorios(){
-
+        $autoparte_acce = $this->autopartes_m->obtenerAccesorios();
+        if (!empty($autoparte_acce)){   
+            $data['accesorios'] = $autoparte_acce;
+            $this->load->view('principal.html',$data);
+        }else{
+            $msjError; 
+            $msjError = "No se encontraron accesorios";
+            return $msjError;
+        } 
     }
 
     /////////////////Autoparte/////////////////
